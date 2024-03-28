d2baa7829f76f6ddc63aa7f74cbd185e1c04dee9
meanlightk@gmail.com <meanlightk@gmail.com>
2024-03-28 14:39:50 +0900

    회원도메인, 회원리포지토리, 회원리포지토리테스트

---
+++ src/test/java/com/meanlightk/study/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.meanlightk.study.repository;
+
+import com.meanlightk.study.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    //  @AfterEach => 메소드 실행이 끝날 때마다 어떤 동작을 하는 것임 (콜백 메소드)
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+
+    }
+
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+        // === Assertions.assertEquals(member, result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member1.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+
+}
