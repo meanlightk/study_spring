84f7267d83c6a11a6ae216189c247e9f90977b64
meanlightk@gmail.com <meanlightk@gmail.com>
2024-03-29 00:45:48 +0900

    H2 데이터베이스 연결

--- src/main/java/com/meanlightk/study/SpringConfig.java
+++ src/main/java/com/meanlightk/study/SpringConfig.java
@@ -1,28 +1,32 @@
 package com.meanlightk.study;
 
+import com.meanlightk.study.repository.JdbcMemberRepository;
 import com.meanlightk.study.repository.MemberRepository;
-import com.meanlightk.study.repository.MemoryMemberRepository;
 import com.meanlightk.study.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
-    /**
-     * 자바 코드로 직접 스프링 빈을 등록할 때 장점
-     * 나중에 MemoryMemberRepository를 DbMemoryMemberRepository로 변경하고싶을때,
-     *  return new MemoryMemberRepository(); ->  return new DbMemoryMemberRepository();
-     * 해주면 됨.
-     */
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 
 }
